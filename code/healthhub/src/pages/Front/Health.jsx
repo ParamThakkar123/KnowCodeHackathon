a31bc0bf878c7cef55c298a68e755b4587f243fa
paramthakkar123 <paramthakkar864@gmail.com>
2024-01-26 22:24:45 +0530

    code added

---
+++ code/healthhub/src/pages/Front/Health.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Health = () => {
+  return (
+    <div className="p-8 h-96">
+      <h1 className="text-2xl text-blue-600">About Healthhub</h1>
+      <p className="mt-2 text-2xl">
+        HealthHub is a one stop solution to all your healthcare needs
+      </p>
+    </div>
+  );
+};
+
+export default Health;
